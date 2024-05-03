30b6c98681de2e09b2e2d55078baa7a5cf966a76
Aniket <aniket@itobuz.com>
2024-05-03 16:03:58 +0530

    add staff

---
+++ cypress/e2e/Manage Staff/staff.cy.js
@@ -0,0 +1,31 @@
+/// <reference types= "cypress" />
+
+import { login } from "../../support/utils";
+import { faker } from '@faker-js/faker';
+import 'cypress-xpath'
+import "cypress-localstorage-commands";
+
+describe('Manage Staff', () => {
+
+  beforeEach(() => {
+    const { Base_URL, ADMIN_Email, ADMIN_Password } = Cypress.env();
+    cy.visit(Base_URL);
+    login(ADMIN_Email, ADMIN_Password);
+    cy.wait(2000);
+    // cy.saveLocalStorage()
+    // cy.getAllLocalStorage()
+  });
+
+  it('Add a staff', () => {
+    cy.xpath('//span[normalize-space()="Manage Staff"]').click()
+    cy.xpath('//div[contains(@title,"staff/delivery partner")]').click()
+    cy.xpath('//input[@name="name"]').type(faker.person.fullName())
+    cy.xpath('//input[@name="phoneNumber"]').type(faker.phone.number())
+    cy.xpath('//div[@class="multi-select__input-container css-19bb58m"]').click()
+    cy.get('#react-select-3-option-1').type("{enter}")
+    cy.xpath('//input[@name="email"]').type(faker.internet.email())
+    cy.xpath('//button[@type="submit"]').click()
+    cy.contains("Staff added successfully.")
+
+  })
+})
