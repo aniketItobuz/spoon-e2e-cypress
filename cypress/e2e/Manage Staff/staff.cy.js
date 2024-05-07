2453fa6dffbfdfb9c6507b5c52c9e3a2a8058873
Aniket <aniket@itobuz.com>
2024-05-07 16:42:30 +0530

    add staff

--- cypress/e2e/Manage Staff/staff.cy.js
+++ cypress/e2e/Manage Staff/staff.cy.js
@@ -7,6 +7,8 @@ import "cypress-localstorage-commands";
 
 describe('Manage Staff', () => {
 
+  const name = faker.person.fullName()
+
   beforeEach(() => {
     const { Base_URL, ADMIN_Email, ADMIN_Password } = Cypress.env();
     cy.visit(Base_URL);
@@ -19,13 +21,20 @@ describe('Manage Staff', () => {
   it('Add a staff', () => {
     cy.xpath('//span[normalize-space()="Manage Staff"]').click()
     cy.xpath('//div[contains(@title,"staff/delivery partner")]').click()
-    cy.xpath('//input[@name="name"]').type(faker.person.fullName())
+    cy.xpath('//input[@name="name"]').type(name)
     cy.xpath('//input[@name="phoneNumber"]').type(faker.phone.number())
     cy.xpath('//div[@class="multi-select__input-container css-19bb58m"]').click()
     cy.get('#react-select-3-option-1').type("{enter}")
     cy.xpath('//input[@name="email"]').type(faker.internet.email())
     cy.xpath('//button[@type="submit"]').click()
     cy.contains("Staff added successfully.")
+  })
 
+  it('Add a staff', () => {
+    cy.xpath('//span[normalize-space()="Manage Staff"]').click()
+    cy.reload()
+    cy.xpath('//input[@placeholder="Search Items"]').type(name)
+    cy.wait(2000)
+    cy.contains(name)
   })
 })
